aa33996ce88ed1cca2b9dfef49d31daf4bf6131b
hassanDurra <hassan2109f@aptechgdn.net>
2023-10-09 02:20:03 +0500

    Home page integerated and The Registeration page too

---
+++ TravioHotel/Controllers/User/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravioHotel.Controllers.User
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Registeration()
+        {
+            return View("Views/User/Register.cshtml");
+        }
+    }
+}
---
+++ TravioHotel/Controllers/User/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TravioHotel.Controllers.User
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+         
+            return View("Views/User/Index.cshtml");
+        }
+    }
+}
