a64a5692062e89cdcb7a58f9478ad241a742e95e
Hassandurra <Hassan2109f@aptechgdn.net>
2023-10-26 13:50:25 +0500

    changes in getting airport iata if there is no city available so its should search by country code

--- TravioHotel/Controllers/User/BookingRequestController.cs
+++ TravioHotel/Controllers/User/BookingRequestController.cs
@@ -18,37 +18,49 @@ namespace TravioHotel.Controllers.User
                 this.httpContext = _httpContext;    
                 this.mailServer  = _mailServer;
         }
-        public async Task<IActionResult> RequestBooking( BookingRequest booking_data)
+        public async Task<IActionResult> RequestBooking(BookingRequest booking_data)
         {
-             string isLogedIn = httpContext.HttpContext.Session.GetString("user") ?? "";
-             ViewBag.isLoggedIn = isLogedIn;
+            string isLogedIn = httpContext.HttpContext.Session.GetString("user") ?? "";
+            ViewBag.isLoggedIn = isLogedIn;
             if (ViewBag.isLoggedIn != "")
             {
+                
+                    var fromCountry = await Database.Cities.Where(e => e.name == booking_data.from_city).FirstOrDefaultAsync();
+                    var toCountry = await Database.Cities.Where(e => e.name == booking_data.to_city).FirstOrDefaultAsync();
 
-                var BookingRequestDetails = new
-                {
-                    from_city = booking_data.from_city,
-                    to_city = booking_data.to_city,
-                    departureDate = booking_data.departure_date,
-                    arrivalDate = booking_data.arrival_date,
-                };
-                ViewBag.BookingDetails = BookingRequestDetails;
+                    var BookingRequestDetails = new
+                    {
+                        from_country  = fromCountry.country_code,
+                        to_country    = toCountry.country_code,
+                        from_city     = booking_data.from_city,
+                        to_city       = booking_data.to_city,
+                        departureDate = booking_data.departure_date,
+                        arrivalDate   = booking_data.arrival_date,
+                    };
+                    ViewBag.BookingDetails = BookingRequestDetails;
                     return View("Views/User/Listing.cshtml");
-             
-            }
 
-          //  TempData["Error"] = "Before Requesting For A Flight Booking You must Login";
-            return View("Views/User/Listing.cshtml");
-       }
+                }
+
+                TempData["Error"] = "Before Requesting For A Flight Booking You must Login";
+                return View("Views/User/Listing.cshtml");
+            
+        }
         //This Function Will get The IATA Code from Airports 
-        public async Task<IActionResult> getAirportsIata(string fromCity , string toCity)
+        public async Task<IActionResult> getAirportsIata(string fromCity , string toCity, string fromCountryIso  , string toCountryIso)
         {
                 var from_city = await Database.Airports.Where(e => e.City_name == fromCity).FirstOrDefaultAsync();
-                var to_city = await Database.Airports.Where(e => e.City_name == toCity).FirstOrDefaultAsync();
-        
-                if(fromCity != null || toCity != null)
+                var to_city   = await Database.Airports.Where(e => e.City_name == toCity).FirstOrDefaultAsync();
+                if(from_city == null || to_city == null)
                 {
-                    var Data  = new {fromCityDetails = from_city ,  toCityDetails = to_city}; 
+                var fromCountry  = await Database.Airports.Where(e => e.Country_iso == fromCountryIso).FirstOrDefaultAsync();
+                var toCountry    = await Database.Airports.Where(e => e.Country_iso == toCountryIso).FirstOrDefaultAsync();
+                var Data = new { fromCountryDetails = fromCountry, toCountryDetails = toCountry, message = "FromCountry" };
+                return Json(Data);
+                }
+                if(fromCity != null && toCity != null)
+                {
+                    var Data  = new {fromCityDetails = from_city ,  toCityDetails = to_city , message = "FromCity"}; 
                     return Json(Data);
                 }
                  var message = new { message = "No Data Found" };
