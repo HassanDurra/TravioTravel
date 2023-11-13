5b57fd2dbb3baf66f574db0b8aa7f2effc998de2
Hassandurra <Hassan2109f@aptechgdn.net>
2023-11-14 00:44:06 +0500

    Booking Dashboard Updates

--- TravioHotel/Controllers/Admin/Bookings.cs
+++ TravioHotel/Controllers/Admin/Bookings.cs
@@ -43,13 +43,110 @@ namespace TravioHotel.Controllers.Admin
 
             if (bookingData != null)
             {
-            
-               
+                var departureDate = requestData["departureDate"];
+                var departureTime = requestData["departureTime"];
+                var arrivalDate = requestData["arrivalDate"];
+                var arrivalTime = requestData["arrivalTime"];
+                // Convert the date strings to DateTime objects
+                DateTime parsedDepartureDate, parsedArrivalDate;
+
+                if (DateTime.TryParse($"{departureDate} {departureTime}", out parsedDepartureDate) &&
+                    DateTime.TryParse($"{arrivalDate} {arrivalTime}", out parsedArrivalDate))
+                {
+                    // Format the departure and arrival dates
+                    var formattedDepartureDate = parsedDepartureDate.ToString("MMM, dd-yyyy");
+                    var formattedDepartureTime = parsedDepartureDate.ToString("h:mm tt");
+                    var formattedArrivalDate   = parsedArrivalDate.ToString("MMM, dd-yyyy");
+                    var formattedArrivalTime   = parsedArrivalDate.ToString("h:mm tt");
+
+
+                    // Update the departure and arrival dates
+                    bookingData.departure_date = formattedDepartureDate;
+                    bookingData.departure_time = formattedDepartureTime;
+                    bookingData.arrival_date = formattedArrivalDate;
+                    bookingData.arrival_time = formattedArrivalTime;
+                    bookingData.created_at   = Convert.ToString(DateTime.UtcNow);
+                    // Save changes to the database
+                    await Database.SaveChangesAsync();
+                    TempData["Success"] = "Ticket Has Been Re-Scheduled";
+                    // Redirect to the "Scheduled" action in the "Booking" controller
+                    return RedirectToAction("FlightsBookings", "Bookings");
+                }
+                else
+                {
+                    TempData["Error"] = "Invalid date(s)";
+                }
+
             }
 
             TempData["Error"] = "Failed To Re-Schedule The Data";
-            return RedirectToAction("Scheduled" , "Booking");
+            return RedirectToAction("Scheduled" , "Bookings");
 
+        }
+
+        // Departed From Origin
+        public async Task<IActionResult> Departed(int Id)
+        {
+            var bookingDetails = await Database.BookingClientDetails.Where(e => e.id == Id).FirstOrDefaultAsync();
+            if(bookingDetails != null)
+            {
+                bookingDetails.status = 1;
+                await Database.SaveChangesAsync();
+                TempData["Success"] = $"{bookingDetails.firstName} {bookingDetails.lastName} Has Been Departed From Origin";
+                return RedirectToAction("FlightsBookings", "Bookings");
+            }
+            TempData["Error"] = "Failed To Update Status";
+            return RedirectToAction("FlightsBookings", "Bookings");
+        }
+
+        //Arrived To Destination
+        public async Task<IActionResult> Arrived(int Id)
+        {
+            var bookingDetails = await Database.BookingClientDetails.Where(e => e.id == Id).FirstOrDefaultAsync();
+            if (bookingDetails != null)
+            {
+                bookingDetails.status = 2;
+                await Database.SaveChangesAsync();
+                TempData["Success"] = $"{bookingDetails.firstName} {bookingDetails.lastName} Has Arrived To Thier Destination";
+                return RedirectToAction("FlightsBookings", "Bookings");
+            }
+            TempData["Error"] = "Failed To Update Status";
+            return RedirectToAction("FlightsBookings", "Bookings");
+        }
+        public async Task<IActionResult> Cancel(int Id , string Status)
+        {
+            var bookingDetails = await Database.BookingClientDetails.Where(e => e.id == Id).FirstOrDefaultAsync();
+            if (bookingDetails != null)
+            {
+               if(Status == "Scheduled")
+                {
+                    bookingDetails.status = 3;
+                    await Database.SaveChangesAsync();
+                    TempData["Success"] = $"Booking For {bookingDetails.firstName} {bookingDetails.lastName} Has been Cancelled";
+                    return RedirectToAction("FlightsBookings", "Bookings");
+                }
+                TempData["Error"] = $"Cannot Cancel Booking For {bookingDetails.firstName} {bookingDetails.lastName} As Flight Has been Departed Or Arrived to its destination ";
+                    return RedirectToAction("FlightsBookings", "Bookings");
+
+            }
+            TempData["Error"] = "Failed To Update Status";
+            return RedirectToAction("FlightsBookings", "Bookings");
+        }
+        public async Task<IActionResult> Remove(int Id , string Status)
+        {
+            var bookingDetails = await Database.BookingClientDetails.Where(e => e.id == Id).FirstOrDefaultAsync();
+            if (bookingDetails != null)
+            {
+                if(Status == "Valid") {             
+                TempData["Success"] = $"Booking Details For {bookingDetails.firstName} {bookingDetails.lastName} Has been Removed";
+                Database.BookingClientDetails.Remove(bookingDetails);
+                return RedirectToAction("FlightsBookings", "Bookings");
+                }
+                TempData["Error"] = TempData["Success"] = $"Failed To Remove Booking Details For {bookingDetails.firstName} {bookingDetails.lastName} as Flight Has been Departed From The Origin";
+                return RedirectToAction("FlightsBookings", "Bookings");
+            }
+            TempData["Error"] = $"Failed To Remove Booking Details For {bookingDetails.firstName} {bookingDetails.lastName}";
+            return RedirectToAction("FlightsBookings", "Bookings");
         }
     }
 }
--- TravioHotel/Program.cs
+++ TravioHotel/Program.cs
@@ -36,6 +36,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Bookings}/{action=FlightsBookings}/{id?}");
 
 app.Run();
